93ed5aa675e9b412a20e9e67f0e240e05c18ceb1
erin-fitzpatric <efitzpatric@gmail.com>
2019-11-05 01:04:59 -0500

    added array practice problem

--- WhiteBoardPractice/src/ui/FirstLast6ArraysApp.java
+++ WhiteBoardPractice/src/ui/FirstLast6ArraysApp.java
@@ -3,9 +3,38 @@ package ui;
 public class FirstLast6ArraysApp {
 
 	public static void main(String[] args) {
-		
-	}
 
+		int nums[] = new int[3];
+
+		nums[0] = 1;
+		nums[1] = 2;
+		nums[2] = 6;
+
+		System.out.println(firstLast6(nums));
+		
+		int numsOne[] = new int[4];
+
+		numsOne[0] = 6;
+		numsOne[1] = 1;
+		numsOne[2] = 2;
+		numsOne[2] = 3;
+
+		System.out.println(firstLast6(numsOne));
+		
+		int numsTwo[] = new int []{13, 6, 1, 2, 3};
+
+		System.out.println(firstLast6(numsTwo));
+	}
+	
+	public static boolean firstLast6(int[] nums) {
+		int last = (nums.length) - 1;
+		if (nums[0] == 6 || nums[last] == 6) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
 }
 
 // DIRECTIONS
